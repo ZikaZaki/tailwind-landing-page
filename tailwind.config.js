951d3e113dbe319e9d651251fe4faebabab429ae
ZikaZaki <zika.github@gmail.com>
2023-01-17 03:34:12 +0300

    Add testmonials section

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
         veryDarkBlue: "hsl(233, 12%, 13%)",
         veryPaleRed: "hsl(13, 100%, 96%)",
         veryLightGray: "hsl(0, 0%, 98%)",
-      },
+      }
     },
   },
   plugins: [],
